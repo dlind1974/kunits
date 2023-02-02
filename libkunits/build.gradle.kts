9447ed6a33059031e15570d2db677468bfeeac4d
Daniel Lind <dlind1974@gmail.com>
2023-02-02 17:49:32 +0100

    Add gradle jar

--- libkunits/build.gradle.kts
+++ libkunits/build.gradle.kts
@@ -6,7 +6,6 @@
  * User Manual available at https://docs.gradle.org/7.6/userguide/building_java_projects.html
  * This project uses @Incubating APIs which are subject to change.
  */
-
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
